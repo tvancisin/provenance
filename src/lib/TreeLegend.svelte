acd57f7790744a491ad26494a3e8303fa8da7a88
tvancisin <tomasvancisin@gmail.com>
2026-03-31 13:58:31 +0100

    tree colors

--- src/lib/TreeLegend.svelte
+++ src/lib/TreeLegend.svelte
@@ -7,23 +7,31 @@
   <g transform={`translate(${60}, ${20})`}>
     <!-- Research -->
     <g transform="translate(0, 0)">
-      <rect x="20" y="-10" width="20" height="20" fill="#800000" />
-      <text x="50" y="2" font-size="14" fill="white" alignment-baseline="middle">Research</text>
+      <rect x="20" y="-10" width="20" height="20" fill="#973535" />
+      <text x="50" y="2" font-size="14" fill="white" alignment-baseline="middle"
+        >Research</text
+      >
     </g>
     <!-- Peacetech tools -->
     <g transform="translate(0, 25)">
-      <rect x="20" y="-10" width="20" height="20" fill="#005266" />
-      <text x="50" y="2" font-size="14" fill="white" alignment-baseline="middle">Peacetech Tools</text>
+      <rect x="20" y="-10" width="20" height="20" fill="#1AD1FF" />
+      <text x="50" y="2" font-size="14" fill="white" alignment-baseline="middle"
+        >Peacetech Tools</text
+      >
     </g>
     <!-- Data transformations -->
     <g transform="translate(0, 50)">
-      <rect x="20" y="-10" width="20" height="20" fill="#003D4D" />
-      <text x="50" y="2" font-size="14" fill="white" alignment-baseline="middle">Data Transformations</text>
+      <rect x="20" y="-10" width="20" height="20" fill="#007B99" />
+      <text x="50" y="2" font-size="14" fill="white" alignment-baseline="middle"
+        >Data Transformations</text
+      >
     </g>
     <!-- Real-life events -->
     <g transform="translate(0, 75)">
-      <rect x="20" y="-10" width="20" height="20" fill="#404040" />
-      <text x="50" y="2" font-size="14" fill="white" alignment-baseline="middle">Real-Life Events</text>
+      <rect x="20" y="-10" width="20" height="20" fill="#8B8B8B" />
+      <text x="50" y="2" font-size="14" fill="white" alignment-baseline="middle"
+        >Real-Life Events</text
+      >
     </g>
   </g>
 </svg>
